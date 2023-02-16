0b59d7d853630cb2e78bb1364d186fa51f4610df
Akinsanmi omobonike <officialraydata@gmail.com>
2023-02-16 09:05:48 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include(#include <stdio.h)
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+printf("size of a char: %i bytes(s)\n", size of(char));
+printf("size of a int: %i bytes(s)\n", size of(int));
+printf("size of a long int: %i bytes(s)\n", size of(long int));
+printf("size of a long long int: %i bytes(s)\n", size of(long long int));
+printf("size of a float: %i bytes(s)\n", size of(float));
+return (0);
+}
