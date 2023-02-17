2501674f9d42e03380012f3d83e8243413df214d
Akinsanmi omobonike <officialraydata@gmail.com>
2023-02-17 06:37:13 -0800

    Assign random number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlibh.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print if number is positive, zero or negative
+ * Return: (Success)
+ *
+*/
+
+int main(void)
+{
+Int n;
+
+Srand(time(0));
+n = rand() - RAND - MAX / 2;
+
+If(n > 0)
+{
+Print f("%d is positive\n", n);
+}
+else if (n == 0)
+{
+Print f("%d is zero\n", n);
+}
+else
+{
+Print f("%d is negative\n", n);
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Alx task - Variables, if, else, while
