6d97058432d82abea4accbdfd0abcd5f80b50bdb
Akinsanmi omobonike <officialraydata@gmail.com>
2023-02-23 02:03:17 -0800

    0-main.c, main.h

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * Return:0
  */
 
 int main(void)
@@ -13,5 +13,4 @@ i = 98;
 positive_or_negative(i);
 
 return (0);
-
 }
